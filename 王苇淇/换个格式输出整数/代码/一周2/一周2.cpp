7d7b8dee59efdd133d7084b972be03cab84df161
王苇淇 <49114227+wwqxka@users.noreply.github.com>
2019-04-03 17:49:19 +0800

    Add files via upload

---
+++ 王苇淇/换个格式输出整数/代码/一周2/一周2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main()
+{
+    int n, a, b, c;
+	cin>>n;
+    a = n / 100;
+    b = n % 100 / 10;
+    c = n % 10;
+    for(int i = 0; i < a; i++)  cout<<"B";
+    for(int i = 0; i < b; i++)  cout<<"S";
+    for(int i = 1; i <= c; i++) cout<<i ;
+
+    return 0;
+}
