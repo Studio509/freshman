4976abe8612afb7a0f4bddcabf4d5c0a999b9a0c
郑玉锦 <49113870+zhengyujin123@users.noreply.github.com>
2019-04-06 10:09:22 +0800

    Create 一元多项式求导.cpp

---
+++ 郑玉锦/一元多项式求导/一元多项式求导.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int c[100],x[100],i=0,j=0;
+	char a;
+	while(1)
+	{
+	   cin>>c[i]>>x[i];
+	   i++;
+	   a=cin.get();
+	   if(a=='\n')
+		   break;
+	}
+	for(j=0;j<i;j++)
+	{
+		if(c[j]==0)
+			cout<<c[j]<<" ";
+		else if(x[j]==0)
+			cout<<x[j]<<" ";
+		else if(x[j]==1)
+			cout<<c[j]*x[j]<<" ";
+		else
+		{
+			cout<<c[j]*x[j]<<" "<<x[j]-1<<" ";
+		}
+	}
+}
