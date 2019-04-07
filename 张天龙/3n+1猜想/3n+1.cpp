f5189686b46abc2569991c178edd96ddcc8b80e1
张天龙 <49112268+ztl123@users.noreply.github.com>
2019-04-07 19:46:57 +0800

    Update 3n+1.cpp

--- 张天龙/3n+1猜想/3n+1.cpp
+++ 张天龙/3n+1猜想/3n+1.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
 
+using namespace std;
+
+void main()
+
+{
+
+  int n,i;
+
+  cout<<"请输入一个小于1000的正整数：";
+
+  cin>>n;
+
+  for(i=1;;i++)
+
+  {
+
+    if(n%2==0)
+
+		  n=n/2;
+
+	  else if(n==1)
+
+		  break;
+
+    else
+
+		  n=(3*n+1)/2;
+
+  }
+
+  cout<<i;
+
+}
