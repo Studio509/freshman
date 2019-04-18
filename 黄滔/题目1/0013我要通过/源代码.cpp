d867b68934903521225ec442ee593a8efee97da6
黄滔 <49026422+Htqo@users.noreply.github.com>
2019-04-18 17:06:26 +0800

    Create 源代码.cpp

---
+++ 黄滔/题目1/0013我要通过/源代码.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	string s[10];
+	int i,j,star=0,size,m,d;
+	  cin>>size;
+	    for(j=0;j<size;j++)
+	      cin>>s[j];
+	    for(j=0;j<size;j++)//大循环
+		{   
+			m=0;star=0;
+			d=s[j].length();
+	       for(i=0;i<d;i++)////第一个条件每个是否含有PAT以外
+	          {
+			   if(s[j][i]=='P'||s[j][i]=='A'||s[j][i]=='T') 
+	             m++;
+			     }
+		     if(m==d)
+		   {   for(i=0;i<d;i++)
+		      {
+				  if(s[j][i]=='P'&&s[j][i+1]=='A'&&s[j][i+2]=='T')//第二个条件
+			        { star=1; break;}
+                  if(s[j][i]=='P'&&s[j][i+1]=='A'&&s[j][i+2]=='A'&&s[j][i+3]=='T')//第二个条件
+			        { star=1; break;}   
+                  if(s[j][i]=='P'&&s[j][i+1]=='A'&&s[j][i+2]=='A'&&s[j][i+3]=='A'&&s[j][i+4]=='T')//第二个条件
+			        { star=1; break;}  
+		         }
+		   }
+           if(star==0)
+		      cout<<"NO"<<endl;
+		   if(star==1)
+               cout<<"YES"<<endl;
+		     }
+	   
+    return 0;
+}
