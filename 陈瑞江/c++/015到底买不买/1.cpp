3b16a9ac1245151b4be3980683f3409d9dd748a5
陈瑞江 <49113364+chenruijiang@users.noreply.github.com>
2019-05-10 17:55:56 +0800

    Update 1.cpp

--- 陈瑞江/c++/015到底买不买/1.cpp
+++ 陈瑞江/c++/015到底买不买/1.cpp
@@ -8,13 +8,13 @@ int m,n,i,j,count=0;
 cin>>a>>b;
 m=a.length();
 n=b.length();
-for(i=1;i<=m;i++)
-	for(j=1;j<=n;j++)
+for(i=0;i<=m;i++)
+	for(j=0;j<=n;j++)
 		if(a[i]==b[j])
 			count++;
 if(count==n)
 cout<<"yes "<<m-count<<endl;
 else
-cout<<"no"<<n-count<<endl;
+cout<<"no "<<n-count<<endl;
 return 0;
 }
