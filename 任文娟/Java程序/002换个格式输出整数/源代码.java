54925a5db0f018389704c4c8c4606279873c8aa4
任文娟 <49113467+renwenjuan@users.noreply.github.com>
2019-05-13 09:01:34 +0800

    Create 源代码.java

---
+++ 任文娟/Java程序/002换个格式输出整数/源代码.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.*;
+import java.io.*;
+
+public class integer {
+
+	public static void main(String[] args) {
+		 int i,j,k,a,b,c;
+		  System.out.println("输出一个不超过3位的正整数:");
+		  Scanner zs=new Scanner(System.in);
+		  int n=zs.nextInt();
+		  a=n/100;
+		  b=n/10%10;
+		  c=n%10;
+		  for(i=1;i<=a;i++)
+		     System.out.print("B");
+		  for(j=1;j<=b;j++)
+		     System.out.print("S");
+		  for(k=1;k<=c;k++)
+			  System.out.print(k);
+	}
+}
