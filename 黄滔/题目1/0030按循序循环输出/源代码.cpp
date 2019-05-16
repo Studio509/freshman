2eb28ac873e5ad2a9732784e136e2065e27c57a0
黄滔 <49026422+Htqo@users.noreply.github.com>
2019-05-16 17:43:50 +0800

    Create 源代码.cpp

---
+++ 黄滔/题目1/0030按循序循环输出/源代码.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+using namespace std; 
+int main() 
+{ 
+	int mapp[128] ={0},c;
+	string a;
+	cin>>a;
+	for(int i=0;i<a.length();i++)
+	{
+		c=a[i];
+		mapp[c]++;
+//while((c=cin.get())!=EOF) 
+	//mapp[c]++; 
+	}
+while(mapp['P']>0||mapp['A']>0||mapp['T']>0||mapp['e']>0||mapp['s']>0||mapp['t']>0)
+{
+	if(mapp['P']-->0) cout<<'P';
+    if(mapp['A']-->0) cout<<'A'; 
+    if(mapp['T']-->0) cout<<'T'; 
+    if(mapp['e']-->0) cout<<'e'; 
+    if(mapp['s']-->0) cout<<'s';
+	if(mapp['t']-->0) cout<<'t';
+}
+return 0; 
+}
