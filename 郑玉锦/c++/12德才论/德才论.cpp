f8e15f8c06585e05a81a372f0d781066b1fb56de
郑玉锦 <49113870+zhengyujin123@users.noreply.github.com>
2019-05-18 22:59:22 +0800

    Update 德才论.cpp

--- 郑玉锦/c++/12德才论/德才论.cpp
+++ 郑玉锦/c++/12德才论/德才论.cpp
@@ -14,13 +14,13 @@ int main()
 	int l;//最低录取分
 	int h;//优先录取分
 	int i;//循环用
+	int k;//循环用
 	student s[100];//定义一个学生类数组
 	int x1=0;student y[4][100];//记录才德全尽的学生
 	int x2=0;student y2[100];//记录德胜才的学生
 	int x3=0;student y3[100];//记录才德皆忘但尚有“德胜才”的学生
 	int x4=0;student y4[100];//记录其他才德达线的学生
 	student st;//之后按分数排序时用
-	int z;//记录德分和才分之和
 	cin>>n;
 	cin>>l;
 	cin>>h;
@@ -38,7 +38,7 @@ int main()
 			y[1][x2]=s[i];
 			x2=x2+1;
 		}
-		else if(s[i].d_score<h&&s[i].d_score>=l&&s[i].c_score>=l&&s[i].c_score<h)
+		else if(s[i].d_score<h&&s[i].d_score>=l&&s[i].c_score>=l&&s[i].c_score<h&&s[i].d_score>s[i].c_score)
 		{
 			y[2][x3]=s[i];
 			x3=x3+1;
@@ -56,14 +56,16 @@ int main()
 		int b;//确定每一类学生输几次
 		if(j==0)
 			b=x1;
-		if(j==1)
+		else if(j==1)
 			b=x2;
-		if(j==2)
+		else if(j==2)
 			b=x3;
-		if(j==3)
+		else if(j==3)
 			b=x4;
-	for(i=0;i<b-1;i++)//给各类学生排序
+	for(k=1;k<b;k++)
 	{
+		for(i=0;i<b-k;i++)//给各类学生排序
+		{
 		if((y[j][i].c_score+y[j][i].d_score)<(y[j][i+1].c_score+y[j][i+1].d_score))//根据德分和才分之和排序
 		{
 			st=y[j][i];
@@ -75,8 +77,8 @@ int main()
 			if(y[j][i].d_score<y[j][i+1].d_score)//则根据德分排序
 			{
 				st=y[j][i];
-				   y[j][i]=y[j][i+1];
-				   y[j][i+1]=st;
+				y[j][i]=y[j][i+1];
+				y[j][i+1]=st;
 			}
 			else if(y[j][i].d_score==y[j][i+1].d_score)//若德分也相同
 			{
@@ -88,7 +90,9 @@ int main()
 				}
 			}
 		}
-	}}
+	}
+	}
+	}
 	system("cls");
 	cout<<m<<endl;//输出总达线人数
 	for(j=0;j<4;j++)//输出各类学生
