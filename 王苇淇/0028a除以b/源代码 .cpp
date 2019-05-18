0a9667388c2c42e8ebd529e3701553fd476c05af
王苇淇 <49114227+wwqxka@users.noreply.github.com>
2019-05-18 20:31:18 +0800

    Create 源代码 .cpp

---
+++ 王苇淇/0028a除以b/源代码 .cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+#include <string>
+
+int main()
+
+{
+
+	string A;
+
+	int B,R,Q,i;
+
+	cin>>A>>B;
+
+	Q=(A[0]-'0')/B;
+
+	if(Q!=0&&A.length()>1||A.length()==1)
+
+		cout<<Q;
+
+	R=(A[0]-'0')%B;
+
+	for(i=1;i<A.length();i++)
+
+	{
+
+		Q=(R*10+A[i]-'0')/B;
+
+		cout<<Q;
+
+		R=(R*10+A[i]-'0')%B;
+
+	}
+
+	cout<<" "<<R<<endl;
+
+	return 0;
+
+}
