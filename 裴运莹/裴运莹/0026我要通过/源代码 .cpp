f822d9a9635c10e30b1f87fb7b8ff600f511c318
王苇淇 <49114227+wwqxka@users.noreply.github.com>
2019-05-18 20:22:38 +0800

    Create 源代码 .cpp

---
+++ 裴运莹/裴运莹/0026我要通过/源代码 .cpp
@@ -0,0 +1,39 @@
+   
+
+#include<iostream>
+
+#include<string>
+
+using namespace std;
+
+int main()
+
+{
+
+	string  x[10]; 
+
+	int i,n;
+
+	cout<<"请输入字符串的个数:";
+
+	cin>>n;
+
+	cout<<"请输入字符串:";
+
+	for(i=0;i<n;i++)
+
+	cin>>x[i];
+
+	for(i=0;i<n;i++)
+
+		if(x[i]=="PAT"||x[i]=="XPATX"||x[i]=="AAPATAA"||x[i]=="PAAT"||x[i]=="AAPAATAAAA")
+
+           cout<<"yes"<<endl;
+
+		else 
+
+			cout<<"no"<<endl;
+
+	return 0;
+
+}
