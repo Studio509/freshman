00fe8870d97cbbf7fe13278ca815bf54e4ff4f06
任文娟 <49113467+renwenjuan@users.noreply.github.com>
2019-05-28 18:05:06 +0800

    Create 源代码.java

---
+++ 任文娟/Java程序/009简单输出整数/源代码.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class coutint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner zhengshu=new Scanner(System.in);
+		int n,i;
+		n=zhengshu.nextInt();
+		for(i=1;i<=n;i++)
+			System.out.println(i);
+		zhengshu.close();
+	}
+
+}
